97302a6a6a2dc2498133f8879c7a33e78843df10
aayushrathor <rathoreaayush84@gmail.com>
2023-08-13 21:37:40 +0530

    feat: add golang lsp and linting

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -38,6 +38,26 @@ local sources = {
         end,
       })
     end
+  end,
+
+  -- go 
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
+  on_attach = function (client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function ()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
   end
 }
 
